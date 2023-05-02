7a1473b381b5887bc87c770808c290a5e7d11686
Scheopfloeffel <georg.polownikow@gmail.com>
2023-05-02 21:16:27 +0200

    Example 1: Added first example usage (with plt.style.context(url)).

---
+++ examples/example1.py
@@ -0,0 +1,17 @@
+#### Example 1: Uses matplotlib.pyplot and applies each style to a demonstration figure consisting of multiple artists
+
+import matplotlib.pyplot as plt
+from style_sheets_reference_func import plot_figure
+
+if __name__ == "__main__":
+
+    style_list = [
+        "https://raw.githubusercontent.com/Schoepfloeffel/mplstyles/main/schoepfloeffel_style_1.mplstyle"
+    ]
+
+    # Plot a demonstration figure
+    for style_label in style_list:
+        with plt.rc_context({"figure.max_open_warning": len(style_list)}):
+            with plt.style.context(style_label):
+                plot_figure(style_label=style_label)
+    plt.show()
